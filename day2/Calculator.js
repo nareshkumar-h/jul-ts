1195bf17a887ac3279067357c864e031dc14dc2b
Naresh Kumar H <nareshkumarh@live.com>
2022-07-28 12:58:27 +0530

    Added calculator program

--- day2/Calculator.js
+++ day2/Calculator.js
@@ -1,9 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Calculator = void 0;
 class Calculator {
+    constructor() {
+        console.log("Calculator constructor");
+    }
+    getTotal(marks) {
+        let total = 0;
+        //todo - implement logic to calculate total
+        for (let mark of marks) {
+            total = total + mark;
+        }
+        return total;
+    }
+    getPercentage(total, noOfSubjects) {
+        let percentage = 0;
+        //todo - implement a logic to calculate percentage
+        percentage = total / noOfSubjects;
+        return percentage;
+    }
 }
-exports.Calculator = Calculator;
+const calculatorObj = new Calculator(); //Create an Object
+const marks = [40, 50, 99, 80]; //input
+let total = calculatorObj.getTotal(marks); //calling a method 
+console.log("Total:" + total);
+let noOfSubjects = marks.length; //size of an array : 3 subjects
+let percentage = calculatorObj.getPercentage(total, noOfSubjects);
+console.log("Percentage:" + percentage);
 // Task: Create a class Calculator
 // Create a constructor
 // Create  a method getTotal(marks) which should return total
