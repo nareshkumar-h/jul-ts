4f4345ed77d334c4af3407e71682579cae53d95e
Naresh Kumar H <nareshkumarh@live.com>
2022-07-28 13:14:52 +0530

    Added test class for calculator

--- day2/TestCalculator.js
+++ day2/TestCalculator.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Calculator_1 = require("./Calculator");
-const calculatorObj = new Calculator_1.Calculator(); //Create an Object
 const marks = [40, 50, 99, 80]; //input
+let noOfSubjects = marks.length; //size of an array : 3 subjects
+const calculatorObj = new Calculator_1.Calculator(); //Create an Object
 let total = calculatorObj.getTotal(marks); //calling a method 
 console.log("Total:" + total);
-let noOfSubjects = marks.length; //size of an array : 3 subjects
 let percentage = calculatorObj.getPercentage(total, noOfSubjects);
 console.log("Percentage:" + percentage);
