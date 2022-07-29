84aac3c36cc5863c08a5a29d689dfe11e3bc3dfd
Naresh Kumar H <nareshkumarh@live.com>
2022-07-29 11:29:09 +0530

    Added test

--- day3/src/test.ts
+++ day3/src/test.ts
@@ -6,8 +6,8 @@ try{
     let name:string = "Naresh";
     console.log("Name:" + name);
     if(name==""){
-        throw new Error("Invalid Name")
-    }
+        throw new Error("Invalid Name");
+    }    
     console.log("Valid Name");
 }
 catch(err:any){
